08f0b88496493c8460d19025bf689e4fd5adfbd2
John Le <nhan.le@savvycomsoftware.com>
2023-04-20 18:13:48 +0700

    init project

---
+++ product-cms/src/main/java/vn/com/product/cms/ProductCmsApplication.java
@@ -0,0 +1,13 @@
+package vn.com.product.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCmsApplication.class, args);
+    }
+
+}
